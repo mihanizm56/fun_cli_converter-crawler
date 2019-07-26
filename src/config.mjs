28679ff96c5fe00a3f6372beb187b23b463ec6f4
MKozhevnikov <mkozhevnikov@technoserv.com>
2019-07-27 02:40:48 +0300

    fix

--- src/config.mjs
+++ src/config.mjs
@@ -1,3 +1,10 @@
+/// paths
+export const PATH_TO_STATIC = path.join(process.cwd(), "public");
+export const PATH_TO_PDF = path.join(PATH_TO_STATIC, "pdf");
+export const PATH_TO_PHOTOS = path.join(PATH_TO_STATIC, "photos");
+export const PATH_TO_SCREENS = path.join(PATH_TO_STATIC, "screenshots");
+
+/// selectors
 export const VK_URL = "https://vk.com";
 export const INPUT_PDF_SELECTOR = '.moxie-shim > input[type="file"]';
 export const PDF_CONVERTER_URL = "https://imagetopdf.com/ru/";
@@ -8,6 +15,8 @@ export const LOGIN_FIELD_VK_SELECTOR = "#index_email";
 export const PASSWORD_FIELD_VK_SELECTOR = "#index_pass";
 export const LOGIN_BUTTON_SELECTOR = "#index_login_button";
 export const MESSAGES_VK_BUTTON_SELECTOR = "a[href='/im']";
-export const timeValueToRepeatScroll = 16;
 export const INPUT_TYPE_FILE_SELECTOR = "input[type=file]";
 export const BUTTON_SEND_VK_SELECTOR = ".im-send-btn_send";
+
+///other
+export const timeValueToRepeatScroll = 16;
--- src/interface.mjs
+++ src/interface.mjs
@@ -2,17 +2,20 @@ import dotenv from "dotenv";
 import PartyMaker from "./api/party-maker.mjs";
 import path from "path";
 import fs from "fs";
+import {
+  PATH_TO_STATIC,
+  PATH_TO_PDF,
+  PATH_TO_PHOTOS,
+  PATH_TO_SCREENS
+} from "./config.mjs";
 
 dotenv.config();
 
-const pathToStatic = path.join(process.cwd(), "public");
-const pathToPDF = path.join(pathToStatic, "pdf");
-const pathToPhotos = path.join(pathToStatic, "photos");
+/// users settings
 const nameOfPDF = "test_name";
-const screensPath = path.join(pathToStatic, "screenshots");
+const chatName = "Миша Кожевников";
 const loginVK = process.env.LOGIN;
 const passwordVK = process.env.PASSWORD;
-const chatName = "Миша Кожевников";
 
 const party = new PartyMaker({
   pathToPDF,
